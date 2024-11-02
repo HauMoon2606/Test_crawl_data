1c16d8f9dbf74f94befe60b901dbbd497c93c43c
HauMoon2606 <conghau266255@gmail.com>
2024-11-02 15:35:16 +0700

    comple crawl_data transcript movie with scrapy template crawl

--- Scrapy/example/example/spiders/transcript.py
+++ Scrapy/example/example/spiders/transcript.py
@@ -6,15 +6,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptSpider(CrawlSpider):
     name = "transcript"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com/movies"]
-
-    rules = (Rule(LinkExtractor(restrict_xpaths="//ul[@class='scripts-list']/li/a[@href]"), callback="parse_item", follow=True),)
-
+    start_urls = ["https://subslikescript.com/movies_letter-X"]
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5,  # Sets a download delay of 0.5 seconds
+    }
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//ul[@class='scripts-list']/li/a[@href]"), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@rel='next'])[1]"), follow=True),
+    )
     def parse_item(self, response):
         article = response.xpath('//article[@class="main-article"]')
         yield {
             "title":article.xpath('./h1/text()').get(),
             "plot":article.xpath('./p/text()').get(),
-            "transcript":article.xpath('./div[@class="full-script"]/text()').getall(),
+            # "transcript":article.xpath('./div[@class="full-script"]/text()').getall(),
             "url":response.url
         }
